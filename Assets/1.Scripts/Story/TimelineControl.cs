6658ee727b528d960b3d500afb5d0aeb91a6de57
rese1740 <rese1748@naver.com>
2025-04-18 13:19:56 +0900

    [feat]보스 타임라인(2)

--- Assets/1.Scripts/Story/TimelineControl.cs
+++ Assets/1.Scripts/Story/TimelineControl.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TimelineEndHandler : MonoBehaviour
 {
     public PlayableDirector timelineDirector;
     public string nextSceneName; // 다음 씬 이름
+
+    public Image reloadingImg;
 
     void Start()
     {
@@ -13,6 +16,23 @@ public class TimelineEndHandler : MonoBehaviour
             timelineDirector.stopped += OnTimelineStopped;
     }
 
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            reloadingImg.fillAmount += Time.deltaTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            reloadingImg.fillAmount = 0;
+        }
+
+        if(reloadingImg.fillAmount >= 1)
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
     void OnTimelineStopped(PlayableDirector director)
     {
         Debug.Log("타임라인 끝났으니 씬 이동!");
